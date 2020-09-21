4096644dd48e665d1d774695775e3927e9a4028f
mohamedalaa253695 <mohamedalaa253695@gihub.com>
2020-09-21 04:18:46 +0200

    finished styling the nav main menu

---
+++ includes/nowPlayingBar.php
@@ -0,0 +1,66 @@
+<div id="nowPlayingBar">
+	
+	<div id="nowPlayingLeft">
+		<span class="albumLink">
+			<img  src="https://tse4.mm.bing.net/th?id=OIP.XSA_E736p0k-fHShxIC8_AAAAA&pid=Api&P=0&w=199&h=174" class="albumArtwork"></img>
+		</span>
+		<div class="trackInfo">
+			<span class="trackName">
+				<span>Happy Birthday</span>
+			</span>
+			<span class="artistName">
+				<span>reece Kenney</span>
+			</span>
+		</div>
+	</div>
+
+	<div id="nowPlayingCenter">
+		<div class="content playerControls">
+			<div class="buttons">
+				<button class="controlButton shuffl" title="shuffl">
+					<img src="assets/images/icons/shuffle.png" alt="shuffle">
+				</button>
+				<button class="controlButton previous" title="previous">
+					<img src="assets/images/icons/previous.png" alt="Previous">
+				</button>
+				<button class="controlButton play" title="play">
+					<img src="assets/images/icons/play.png" alt="play">
+				</button>
+					<button class="controlButton pause" title="pause" style="display: none;">
+					<img src="assets/images/icons/pause.png" alt="pause">
+				</button>
+				<button class="controlButton next" title="next">
+					<img src="assets/images/icons/next.png" alt="next">
+				</button>
+				<button class="controlButton repeat" title="repeat">
+					<img src="assets/images/icons/repeat.png" alt="repeat">
+				</button>
+			
+			</div>
+			<div class="playbackBar">
+				<span class="progressTime current">0.00</span>
+				<div class="progressBar">
+					<div class="progressBarBg">
+						<div class="progress"></div>
+					</div>
+				</div>
+				<span class="progressTime remaining">0.00</span>
+			</div>
+		</div>
+	</div>
+
+	<div id="nowPlayingRight">
+		<div class="volumeBar">
+			<button class="controlButton volume" title="Volune button">
+				<img src="assets/images/icons/volume.png" alt="Volume">
+			</button>
+			<div class="progressBar">
+					<div class="progressBarBg">
+						<div class="progress"></div>
+					</div>
+			</div>
+
+		</div>
+	</div>
+
+</div>	
