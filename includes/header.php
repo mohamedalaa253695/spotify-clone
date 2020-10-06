33875b54c232c0c4c19566d19fec8386f4539403
mohamedalaa253695 <mohamedalaa253695@gihub.com>
2020-10-06 17:29:42 +0200

    added settings page and user details page

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,25 @@
 <?php 
 
 include("includes/config.php");
+include("includes/classes/User.php");
 include("includes/classes/Artist.php");
 include("includes/classes/Album.php");
 include("includes/classes/Song.php");
-
+include("includes/classes/Playlist.php");
 
 //session_destroy();
 if(isset($_SESSION['userLoggedIn']))
 {
-	$userLoggedIn=$_SESSION['userLoggedIn'];
-	echo "<script>userLoggedIn ='$userLoggedIn'; </script>";
+	$userLoggedIn = new User($conn,$_SESSION['userLoggedIn']);
+	$username 	  = $userLoggedIn->getUsername();
+	echo "<script>userLoggedIn ='$username'; </script>";
 
 }
 else
 {
 	header("Location:register.php");
 }
+
 
 
 
